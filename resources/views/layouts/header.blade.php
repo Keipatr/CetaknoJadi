e37303c4b9162d8470d77cd6d309329ec1e3afc6
YvesKingC <ychandra02@student.ciputra.ac.id>
2023-05-27 20:14:14 +0700

    utk skrg gini dlu. blm bisa di cek krn blm login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -241,17 +241,13 @@
                                 </svg>
                             </a>
                         </div> --}}
-                        <a href="#!" class="text-muted" data-bs-toggle="modal" 
-@if (Auth::check())
-    data-bs-target="#dropdown-menu"
-@else
-    data-bs-target="#modal-1"
-@endif>
-    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
-        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-    </svg>
-</a>
+                        <a href="#!" class="text-muted" data-bs-toggle="modal" data-bs-target="#{{ Auth::check() ? 'dropdown-menu' : 'modal-1' }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user">
+                                <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </a>
+                        
 
 @if (Auth::check())
     <!-- Code for the post-login dropdown menu -->
